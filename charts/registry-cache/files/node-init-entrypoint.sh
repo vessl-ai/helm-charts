e4586ca67bb32c78f3a3990ed12f9888ac431a2f
Amelia Shin <amelia@vessl.ai>
2024-07-01 13:41:16 +0900

    Add registry cache

---
+++ charts/registry-cache/files/node-init-entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Starting node init script."
+pip install tomli==2.0.1 tomli-w==1.0.0 && \
+python3 /opt/vessl/node-init.py
+echo "Finished node init script with exit code $?."
+sleep infinity
