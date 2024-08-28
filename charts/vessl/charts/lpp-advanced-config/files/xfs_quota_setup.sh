6272c876a04dc606ce7b0c2574f007f993d4e61e
Amelia Shin <amelia@vessl.ai>
2024-08-28 15:13:48 +0900

    Use /bin/echo

--- charts/vessl/charts/lpp-advanced-config/files/xfs_quota_setup.sh
+++ charts/vessl/charts/lpp-advanced-config/files/xfs_quota_setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 set -eu
 
 PROJ_NAME=$(basename "$VOL_DIR")
@@ -6,33 +6,33 @@ PROJ_ID=$(od -An -N4 -t u4 < /dev/urandom | tr -d ' ')
 XFS_NAME=$(dirname "$VOL_DIR")
 
 _install_xfsprogs() {
-    echo -e "\033[1;32mInstalling xfsprogs...\033[0m"
+    /bin/echo -e "\033[1;32mInstalling xfsprogs...\033[0m"
     apt-get update
     DEBIAN_FRONTEND=noninteractive apt-get install -y xfsprogs
 }
 
 _create_dir() {
-    echo -e "\033[1;32mCreating directory\033[0m: ${VOL_DIR}"
+    /bin/echo -e "\033[1;32mCreating directory\033[0m: ${VOL_DIR}"
     mkdir -m 0777 -p "$VOL_DIR"
 }
 
 _setup_xfs_quota() {
-    echo -e "\033[1;32mCreating project...\033[0m (ID: ${PROJ_ID}, name: ${PROJ_NAME})"
-    echo "${PROJ_ID}:${VOL_DIR}" >> /etc/projects
-    echo "${PROJ_NAME}:${PROJ_ID}" >> /etc/projid
+    /bin/echo -e "\033[1;32mCreating project...\033[0m (ID: ${PROJ_ID}, name: ${PROJ_NAME})"
+    /bin/echo "${PROJ_ID}:${VOL_DIR}" >> /etc/projects
+    /bin/echo "${PROJ_NAME}:${PROJ_ID}" >> /etc/projid
 
     if ! [[ -v "XFS_QUOTA_SIZE" ]]
     then
-        echo -e "\033[1;31mThe shell variable 'XFS_QUOTA_SIZE' is not set!\033[0m"
-        echo -e "It is likely that something is wrong with the chart configuration."
-        echo -e "Defaulting to 10 GB."
+        /bin/echo -e "\033[1;31mThe shell variable 'XFS_QUOTA_SIZE' is not set!\033[0m"
+        /bin/echo -e "It is likely that something is wrong with the chart configuration."
+        /bin/echo -e "Defaulting to 10 GB."
         XFS_QUOTA_SIZE=10g
     fi
 
-    echo -e "\033[1;32mRunning xfs_quota commands...\033[0m"
+    /bin/echo -e "\033[1;32mRunning xfs_quota commands...\033[0m"
     xfs_quota -x -c "project -s ${PROJ_NAME}"
-    xfs_quota -x -c "limit -p bhard=${XFS_QUOTA_SIZE} ${PROJ_NAME}" ${XFS_NAME}
-    xfs_quota -x -c "report -pbih" ${XFS_NAME}
+    xfs_quota -x -c "limit -p bhard=${XFS_QUOTA_SIZE} ${PROJ_NAME}" "${XFS_NAME}"
+    xfs_quota -x -c "report -pbih" "${XFS_NAME}"
 }
 
 ##################
@@ -43,5 +43,5 @@ _install_xfsprogs
 _create_dir
 _setup_xfs_quota
 
-echo -e "\033[1;32mSetup complete!\033[0m"
+/bin/echo -e "\033[1;32mSetup complete!\033[0m"
 sleep 10 # TODO: remove me
--- charts/vessl/charts/lpp-advanced-config/files/xfs_quota_teardown.sh
+++ charts/vessl/charts/lpp-advanced-config/files/xfs_quota_teardown.sh
@@ -5,23 +5,23 @@ PROJ_NAME=$(basename "$VOL_DIR")
 XFS_NAME=$(dirname "$VOL_DIR")
 
 _install_xfsprogs() {
-    echo -e "\033[1;32mInstalling xfsprogs...\033[0m"
+    /bin/echo -e "\033[1;32mInstalling xfsprogs...\033[0m"
     apt-get update
     DEBIAN_FRONTEND=noninteractive apt-get install -y xfsprogs
 }
 
 _remove_xfs_quota() {
-    echo -e "\033[1;32mRunning xfs_quota commands...\033[0m"
+    /bin/echo -e "\033[1;32mRunning xfs_quota commands...\033[0m"
     xfs_quota -x -c "limit -p bhard=0 ${PROJ_NAME}" ${XFS_NAME}
     xfs_quota -x -c "report -pbih" ${XFS_NAME}
 
-    echo -e "\033[1;32mRemoving project...\033[0m (name: ${PROJ_NAME})"
-    echo "$(sed "/${PROJ_NAME}/d" /etc/projects)" > /etc/projects
-    echo "$(sed "/${PROJ_NAME}/d" /etc/projid)" > /etc/projid
+    /bin/echo -e "\033[1;32mRemoving project...\033[0m (name: ${PROJ_NAME})"
+    /bin/echo "$(sed "/${PROJ_NAME}/d" /etc/projects)" > /etc/projects
+    /bin/echo "$(sed "/${PROJ_NAME}/d" /etc/projid)" > /etc/projid
 }
 
 _remove_dir() {
-    echo -e "\033[1;32mRemoving directory:\033[0m ${VOL_DIR}"
+    /bin/echo -e "\033[1;32mRemoving directory:\033[0m ${VOL_DIR}"
     rm -rf "$VOL_DIR"
 }
 
@@ -33,5 +33,5 @@ _install_xfsprogs
 _remove_xfs_quota
 _remove_dir
 
-echo -e "\033[1;32mTeardown complete!\033[0m"
+/bin/echo -e "\033[1;32mTeardown complete!\033[0m"
 sleep 10 # TODO: remove me
