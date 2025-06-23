914b0f6466255462956242fd102a7149d75fe823
Joon <jshong0907@gmail.com>
2025-06-23 17:26:29 +0900

    check disk pressure

--- charts/vessl/charts/lpp-advanced-config/files/xfs_quota_setup.sh
+++ charts/vessl/charts/lpp-advanced-config/files/xfs_quota_setup.sh
@@ -13,6 +13,25 @@ _create_dir() {
 _setup_quota() {
     /bin/echo -e "\033[1;32mSetting up quota...\033[0m"
     VOL_SIZE_MB=$((VOL_SIZE_BYTES / 1024 / 1024))
+
+    read TOTAL_MB AVAIL_MB < <(
+      df --output=size,avail --block-size=1M "$VOL_DIR" \
+        | tail -n1
+    )
+    /bin/echo "Partition Total: ${TOTAL_MB}MiB, Avail: ${AVAIL_MB}MiB"
+    MIN_FREE_MB=$(( TOTAL_MB * 25 / 100 ))
+    /bin/echo "Require at least 25% free: ${MIN_FREE_MB}MiB"
+
+    if (( VOL_SIZE_MB > AVAIL_MB )); then
+        /bin/echo -e "\033[1;31mError: Not enough space (${AVAIL_MB}MiB available, ${VOL_SIZE_MB}MiB needed)\033[0m"
+        exit 1
+    fi
+
+    if (( AVAIL_MB - VOL_SIZE_MB < MIN_FREE_MB )); then
+        /bin/echo -e "\033[1;31mError: Would leave only $(( AVAIL_MB - VOL_SIZE_MB ))MiB free (<25% of ${TOTAL_MB}MiB)\033[0m"
+        exit 1
+    fi
+
     /bin/echo -e "\033[1;32mCreating image file\033[0m: /opt/local-path-provisioner/${PROJ_NAME}.img"
     fallocate -l ${VOL_SIZE_MB}M /opt/local-path-provisioner/${PROJ_NAME}.img
     sync
