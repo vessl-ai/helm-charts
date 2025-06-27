998e20a860e0bc93f7459a01f90047ffba5a721d
Joon <jshong0907@gmail.com>
2025-06-27 15:05:58 +0900

    remove set +e in remove quota

--- charts/vessl/charts/lpp-advanced-config/files/quota_teardown.sh
+++ charts/vessl/charts/lpp-advanced-config/files/quota_teardown.sh
@@ -5,14 +5,10 @@ PROJ_NAME=$(basename "$VOL_DIR")
 IMAGE_FILE="${VOL_DIR_PARENT}/${PROJ_NAME}.img"
 
 _remove_quota() {
-    set +e
-
     /bin/echo -e "\033[1;32mRemoving quota...\033[0m"
     umount -d "$VOL_DIR"
     rm -f "$IMAGE_FILE"
     sed -i "\|${IMAGE_FILE}|d" /etc/fstab
-
-    set -e
 }
 
 _remove_dir() {
