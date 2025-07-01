c9840b54486e59e953d45cc0d3a769c4f48bff9c
Joon <jshong0907@gmail.com>
2025-07-01 17:43:35 +0900

    [VSSL-12753] Loop device mount concurrency error (#98)
    * use flock
    
    * fix trap use EXIT
    
    * fix indent err

--- charts/vessl/charts/lpp-advanced-config/files/quota_teardown.sh
+++ charts/vessl/charts/lpp-advanced-config/files/quota_teardown.sh
@@ -6,9 +6,12 @@ IMAGE_FILE="${VOL_DIR_PARENT}/${PROJ_NAME}.img"
 
 _remove_quota() {
     /bin/echo -e "\033[1;32mRemoving quota...\033[0m"
-    umount -d "$VOL_DIR"
+    umount -d "$VOL_DIR" 2>/dev/null || true
     rm -f "$IMAGE_FILE"
-    sed -i "\|${IMAGE_FILE}|d" /etc/fstab
+
+    flock -w 10 -e 200 -c "
+        /bin/echo \"\$(sed \"/${PROJ_NAME}/d\" /etc/fstab)\" > /etc/fstab
+    "
 }
 
 _remove_dir() {
